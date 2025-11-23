4c32d5de5b078885de3c774865d681c4f7c9355e
thangvh2004 <thangvh2004@gmail.com>
2025-11-23 12:34:24 +0700

    Hoàn tất database

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:ecmobile/theme/app_colors.dart';
 import 'package:ecmobile/layouts/main_layout.dart';
-
+// 1. IMPORT FILE MỚI
+import 'package:ecmobile/utils/seed_customer.dart';
 // 1. QUAN TRỌNG: Import file chứa hàm nạp dữ liệu bạn vừa tạo
 // (Đảm bảo bạn đã tạo file lib/utils/seed_data.dart và dán code tôi gửi ở tin nhắn trước)
 import 'package:ecmobile/utils/seed_data.dart';
@@ -19,6 +20,7 @@ void main() async {
   // Khi chạy App, nó sẽ thực thi hàm này để bắn dữ liệu lên Firebase
   print("--- BẮT ĐẦU NẠP DỮ LIỆU ---");
   await seedRealData();
+  await seedCustomerSystem();
   print("--- KẾT THÚC NẠP DỮ LIỆU ---");
 
   runApp(const MyApp());
